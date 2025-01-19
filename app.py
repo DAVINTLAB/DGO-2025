82999adfeaca2803ae33cce639f0a32be8d8914a
Stefano Carraro <stefano.c@edu.pucrs.br>
2025-01-19 13:39:06 -0300

    Supports multiple csv files aswell as default button

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from pages import landing_page
+from app_pages import landing_page
+from app_pages.analyzed_comments.analyzed_upload import analyzed_upload
 import streamlit as st
 
 st.set_page_config(
     page_title='Views',
     page_icon='📊',
-    layout='wide'
+    layout='wide',
+    menu_items={'Get Help': None, 'Report a Bug': None, 'About': None}
 )
 
 main_section = st.sidebar.selectbox('Main Section', ['Analyzed Comments', 'Default Comments'])
@@ -15,7 +17,9 @@ if(main_section == 'Analyzed Comments'):
     sub_section = st.sidebar.selectbox('Sub Section', ['Upload File'])
     match sub_section:
         case 'Upload File':
-            pass
+            analyzed_upload()
+        case _:
+            landing_page()
 
 
 
@@ -24,6 +28,8 @@ elif(main_section == 'Default Comments'):
     match sub_section:
         case 'Upload File':
             pass
+        case _:
+            landing_page()
 
 else:
     landing_page()
--- app_pages/analyzed_comments/analyzed_upload.py
+++ app_pages/analyzed_comments/analyzed_upload.py
@@ -1,17 +1,32 @@
 import streamlit as st
 import pandas as pd
 
-def analyzed_upload():    
+def analyzed_upload():
+    DEFAULT_PATHS = {
+        "CNN": "mocks/analyzed/CNN_#Desempate.csv",
+        "FolhaSP": "mocks/analyzed/FolhaSP_#Desempate.csv",
+        "Itatiaia": "mocks/analyzed/Itatiaia_#Desempate.csv",
+        "Poder360": "mocks/analyzed/Poder360_#Desempate.csv",
+        "UOL": "mocks/analyzed/UOL_#Desempate.csv"
+    }
+
     if "analyzed_csv_files" not in st.session_state:
         st.session_state.analyzed_csv_files = {}
 
     st.title('Csv Files Handler')
 
+    if st.button('Upload default'):
+        for name, path in DEFAULT_PATHS.items():
+            try:
+                st.session_state.analyzed_csv_files[name] = pd.read_csv(path, delimiter=';')
+            except Exception:
+                pass
+
     #Upload
     analyzed_files = st.file_uploader('Dump analyzed csv files here', type='csv', accept_multiple_files =True)
     if analyzed_files:
         for file in analyzed_files:
-            st.session_state.analyzed_csv_files[file.name] = pd.read_csv(file)
+            st.session_state.analyzed_csv_files[file.name] = pd.read_csv(file, delimiter=';')
     
     #List and handle changes for uploaded archieves
     if st.session_state.analyzed_csv_files:
@@ -19,8 +34,7 @@ def analyzed_upload():
         st.dataframe(st.session_state.analyzed_csv_files[file_name])
 
         if(st.button('Remove file')):
-            del st.session_state[file_name]
-            st.experimental_rerun()
+            del st.session_state.analyzed_csv_files[file_name]
     
     else:
         st.info('No csv archieve')
